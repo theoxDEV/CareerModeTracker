be17192e40005bbbcc3edd8b876748b93beb8fa3
Matheus Nascimento <56891192+theoxDEV@users.noreply.github.com>
2024-06-05 20:34:35 -0300

    Add project files.

---
+++ CareerModeTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CareerModeTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
